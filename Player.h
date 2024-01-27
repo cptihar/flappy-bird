aff65c3353f2405be5a71945f9755f216332e706
at0mb0mb4 <42113144+cptihar@users.noreply.github.com>
2024-01-27 20:25:21 +0100

    Update Player.h

--- Player.h
+++ Player.h
@@ -4,9 +4,9 @@
 #include <SFML/Graphics.hpp>
 #include <memory>
 
-#define GRAVITY				  0.5f
+#define GRAVITY		      0.5f
 #define DEFAULT_JUMP_SPEED  -12.0f
-#define PATH_TO_SPRITE		"res/BirdImage.png"
+#define PATH_TO_SPRITE	    "res/BirdImage.png"
 
 class Player
 {
@@ -14,11 +14,11 @@ class Player
 		Player(const sf::Vector2u WindowSize);
 		~Player();
 
-		void			   renderPlayer(sf::RenderTarget& target);
-		void			   updatePlayer(const bool SpacePressed, const bool dead);
+		void		   renderPlayer(sf::RenderTarget& target);
+		void		   updatePlayer(const bool SpacePressed, const bool dead);
 		const sf::Sprite&  getPlayerSprite() { return m_BirdSprite; }
-		const float		   getGroundLevel() { return m_groundLevel; }
-		bool&		       getPlayerState() { return m_fucked; }
+		const float	   getGroundLevel() { return m_groundLevel; }
+		bool&		   getPlayerState() { return m_fucked; }
 		sf::Vector2f       getPlayArea();
 		
 		
@@ -27,16 +27,16 @@ class Player
 	private:
 		// Variables
 		std::unique_ptr<sf::Texture>  m_BirdTexture;
-		sf::Sprite				      m_BirdSprite;
-		const sf::Vector2u			  m_WindowSize;
-		float						  m_birdSpeed;
-		const float					  m_groundLevel;
-		bool						  m_fucked;
+		sf::Sprite		      m_BirdSprite;
+		const sf::Vector2u	      m_WindowSize;
+		float			      m_birdSpeed;
+		const float		      m_groundLevel;
+		bool			      m_fucked;
 
 	private:
 		// Functions
-		void		 m_initBirdSprite();
-		void		 m_setDefaultBirdPosition();
+		void	     m_initBirdSprite();
+		void	     m_setDefaultBirdPosition();
 		inline bool  m_isOutOfBounds();
 };
 #endif // !PLAYER_H
